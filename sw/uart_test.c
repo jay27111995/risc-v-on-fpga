d4a4350c55d36f0e55cdb502b0930c14f68063e3
eelljay <jayendra.ellamathy@ericsson.com>
2026-09-20 01:30:54 +0200

    Step 2: test RX_READY polling

--- sw/uart_test.c
+++ sw/uart_test.c
@@ -4,13 +4,35 @@
 #include "uart.h"
 
 int main(void) {
-    // Just send Hi and halt - no RX at all
+    // TX works - now test RX_READY polling
     uart_putc('H');
     uart_putc('i');
     uart_putc('!');
     uart_putc('\n');
     
-    // Halt
+    // Print what CPU sees for RX_READY (as hex digit)
+    uart_putc('R');
+    uart_putc('X');
+    uart_putc('=');
+    uart_putc('0' + (RX_READY & 0xF));
+    uart_putc('\n');
+    
+    // Now wait for RX_READY to become 1
+    uart_putc('W');
+    uart_putc('a');
+    uart_putc('i');
+    uart_putc('t');
+    uart_putc('\n');
+    
+    while (!RX_READY);  // Block here
+    
+    // If we get here, RX_READY was set
+    uart_putc('G');
+    uart_putc('o');
+    uart_putc('t');
+    uart_putc('!');
+    uart_putc('\n');
+    
     while (1);
     return 0;
 }
