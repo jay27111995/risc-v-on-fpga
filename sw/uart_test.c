de61417908dad8aeb847aeb75f29bb384b58e13e
eelljay <jayendra.ellamathy@ericsson.com>
2026-09-20 00:41:42 +0200

    Fix uart_test: use char literals (strings in IMEM can't be loaded)
    The CPU has separate IMEM/DMEM at same address. String literals
    go to .rodata in IMEM, but CPU loads read from DMEM. Use char
    literals as a workaround until we implement proper data copying.

--- sw/uart_test.c
+++ sw/uart_test.c
@@ -3,23 +3,37 @@
 
 #include "uart.h"
 
+// Can't use string literals directly - they're in IMEM but loads read from DMEM
+// So we write characters directly
+void print_hello(void) {
+    uart_putc('H');
+    uart_putc('e');
+    uart_putc('l');
+    uart_putc('l');
+    uart_putc('o');
+    uart_putc(' ');
+    uart_putc('f');
+    uart_putc('r');
+    uart_putc('o');
+    uart_putc('m');
+    uart_putc(' ');
+    uart_putc('R');
+    uart_putc('I');
+    uart_putc('S');
+    uart_putc('C');
+    uart_putc('-');
+    uart_putc('V');
+    uart_putc('!');
+    uart_putc('\n');
+}
+
 int main(void) {
-    uart_puts("Hello from RISC-V!\n");
-    uart_puts("Type something and press Enter:\n");
-
-    char buf[64];
+    print_hello();
+    
+    // Simple echo loop - just echo single characters
     while (1) {
-        int len = uart_read(buf, sizeof(buf) - 1);
-        buf[len] = '\0';
-
-        uart_puts("You said: ");
-        uart_puts(buf);
-        uart_puts("\n");
-
-        // Echo the length too
-        uart_puts("Length: ");
-        uart_put_int(len);
-        uart_puts(" bytes\n");
+        char c = uart_getc();
+        uart_putc(c);
     }
 
     return 0;
