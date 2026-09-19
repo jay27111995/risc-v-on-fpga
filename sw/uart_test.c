2a01a639e91600c84aabe252010c4e9e815737f0
eelljay <jayendra.ellamathy@ericsson.com>
2026-09-20 01:29:14 +0200

    Step 1: simplest TX test - just print Hi and halt

--- sw/uart_test.c
+++ sw/uart_test.c
@@ -4,31 +4,13 @@
 #include "uart.h"
 
 int main(void) {
-    // Send greeting
+    // Just send Hi and halt - no RX at all
     uart_putc('H');
     uart_putc('i');
     uart_putc('!');
     uart_putc('\n');
     
-    // Echo loop - receive chars, send them back
-    while (1) {
-        // Wait for RX_READY
-        while (!RX_READY);
-        
-        // Get length and acknowledge
-        int len = RX_LEN;
-        RX_READY = 0;
-        
-        // Echo back all received characters (read word by word)
-        for (int i = 0; i < len; i++) {
-            // Read word-aligned, extract byte
-            volatile unsigned int *rx_words = (volatile unsigned int *)0x300;
-            unsigned int word = rx_words[i / 4];
-            char c = (word >> ((i % 4) * 8)) & 0xFF;
-            uart_putc(c);
-        }
-        uart_putc('\n');
-    }
-
+    // Halt
+    while (1);
     return 0;
 }
