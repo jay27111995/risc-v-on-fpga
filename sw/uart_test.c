1d132ff34e93cb569d51156b35f41a58bf934ac1
eelljay <jayendra.ellamathy@ericsson.com>
2026-09-20 01:39:29 +0200

    Simplest test: write/read 0x404

--- sw/uart_test.c
+++ sw/uart_test.c
@@ -4,63 +4,30 @@
 #include "uart.h"
 
 int main(void) {
-    uart_putc('H');
-    uart_putc('i');
+    // Use direct address, not the macro
+    volatile unsigned int *rx_ready_ptr = (volatile unsigned int *)0x404;
+    
+    uart_putc('A');
     uart_putc('\n');
     
-    // EXPLICITLY clear RX_READY from CPU side
-    RX_READY = 0;
+    // Write 0
+    *rx_ready_ptr = 0;
     
-    // Read it back multiple times
-    uart_putc('1');
-    uart_putc(':');
-    uart_putc('0' + (RX_READY & 0xF));
+    uart_putc('B');
     uart_putc('\n');
     
-    uart_putc('2');
-    uart_putc(':');
-    uart_putc('0' + (RX_READY & 0xF));
-    uart_putc('\n');
-    
-    uart_putc('3');
-    uart_putc(':');
-    uart_putc('0' + (RX_READY & 0xF));
-    uart_putc('\n');
-    
-    // Now wait for input
-    uart_putc('W');
-    uart_putc('\n');
-    
-    int count = 0;
-    while (!RX_READY) {
-        count++;
-        if (count > 1000000) {
-            uart_putc('.');
-            count = 0;
-        }
-    }
-    
-    uart_putc('G');
-    uart_putc('o');
-    uart_putc('t');
-    uart_putc('\n');
-    
-    unsigned int len = RX_LEN;
-    uart_putc('L');
+    // Read back
+    unsigned int val = *rx_ready_ptr;
+    uart_putc('V');
     uart_putc('=');
-    uart_putc('0' + (len & 0xF));
+    uart_putc('0' + (val & 0xF));
     uart_putc('\n');
     
-    volatile unsigned int *dmem = (volatile unsigned int *)0;
-    unsigned int word = dmem[0x300 / 4];
-    char c = word & 0xFF;
-    uart_putc('[');
-    uart_putc(c);
-    uart_putc(']');
+    uart_putc('C');
     uart_putc('\n');
     
-    RX_READY = 0;
+    // Spin forever
+    while(1);
     
-    while (1);
     return 0;
 }
