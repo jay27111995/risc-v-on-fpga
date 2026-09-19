613caf53b373d4a73bd36ae7a3081e286c5abfe8
eelljay <jayendra.ellamathy@ericsson.com>
2026-09-20 01:41:07 +0200

    Debug: read 5 times, use do-while

--- sw/uart_test.c
+++ sw/uart_test.c
@@ -7,30 +7,29 @@ int main(void) {
     volatile unsigned int *rx_ready_ptr = (volatile unsigned int *)0x404;
     
     uart_putc('A');
-    uart_putc('\n');
     
-    // Write 0
     *rx_ready_ptr = 0;
     
     uart_putc('B');
-    uart_putc('\n');
     
-    // Read back
-    unsigned int val = *rx_ready_ptr;
-    uart_putc('V');
-    uart_putc('=');
-    uart_putc('0' + (val & 0xF));
-    uart_putc('\n');
-    
-    // Now the critical test - while loop
-    uart_putc('W');
-    uart_putc('\n');
-    
-    while (!(*rx_ready_ptr)) {
-        // Spin
+    // Read in loop and print each time
+    for (int i = 0; i < 5; i++) {
+        unsigned int val = *rx_ready_ptr;
+        uart_putc('0' + (val & 0xF));
     }
+    uart_putc('\n');
+    
+    uart_putc('W');
+    
+    // Manual polling loop
+    unsigned int ready;
+    do {
+        ready = *rx_ready_ptr;
+    } while (ready == 0);
     
     uart_putc('X');
+    uart_putc('=');
+    uart_putc('0' + (ready & 0xF));
     uart_putc('\n');
     
     while(1);
