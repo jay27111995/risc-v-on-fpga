975a61b672f9324f9d03281ea0091eadbc991929
eelljay <jayendra.ellamathy@ericsson.com>
2026-09-20 11:46:26 +0200

    Read 2 chars manually, no loop

--- sw/uart_test.c
+++ sw/uart_test.c
@@ -9,17 +9,19 @@ int main(void) {
     // Wait for RX_READY
     while (!RX_READY);
     
-    // Get length and echo all chars
-    int len = RX_LEN;
+    // Read first 2 chars manually (no loop)
     volatile unsigned int *rx_buf = (volatile unsigned int *)0x300;
-    for (int i = 0; i < len; i++) {
-        uart_putc(rx_buf[i] & 0xFF);
-    }
-    uart_putc('\n');
+    char c0 = rx_buf[0] & 0xFF;
+    char c1 = rx_buf[1] & 0xFF;
     
     // Clear
     RX_READY = 0;
     RX_LEN = 0;
+    
+    // Echo them
+    uart_putc(c0);
+    uart_putc(c1);
+    uart_putc('\n');
     
     while(1);
     return 0;
