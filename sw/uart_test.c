d8bd0e2a13b803ce559bb102b8cdde2ddbf4ec70
eelljay <jayendra.ellamathy@ericsson.com>
2026-09-20 11:34:48 +0200

    uart_test: receive and echo input once

--- sw/uart_test.c
+++ sw/uart_test.c
@@ -4,10 +4,24 @@
 #include "uart.h"
 
 int main(void) {
-    uart_putc('H');
-    uart_putc('i');
-    uart_putc('!');
+    uart_puts("Hello! Type something:\n");
+    
+    // Wait for input
+    while (!uart_rx_ready());
+    
+    // Read and echo
+    int len = RX_LEN;
+    uart_puts("You typed: ");
+    for (int i = 0; i < len; i++) {
+        uint32_t word = *(volatile unsigned int *)(0x300 + i * 4);
+        uart_putc(word & 0xFF);
+    }
     uart_putc('\n');
+    
+    // Clear RX
+    RX_READY = 0;
+    
+    uart_puts("Done!\n");
     
     while(1);
     return 0;
