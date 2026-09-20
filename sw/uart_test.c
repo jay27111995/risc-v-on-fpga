4cfca14e570c2eceed39039afcde625e31a8bfa0
eelljay <jayendra.ellamathy@ericsson.com>
2026-09-20 11:42:06 +0200

    Simplest TX test - just Hi

--- sw/uart_test.c
+++ sw/uart_test.c
@@ -4,27 +4,9 @@
 #include "uart.h"
 
 int main(void) {
-    uart_puts("Hello! Type something:\n");
-    
-    // Wait for input
-    while (!uart_rx_ready());
-    
-    // Read and echo
-    int len = RX_LEN;
-    uart_puts("You typed ");
-    uart_put_int(len);
-    uart_puts(" chars: ");
-    volatile unsigned int *rx_buf = (volatile unsigned int *)0x300;
-    for (int i = 0; i < len; i++) {
-        uart_putc(rx_buf[i] & 0xFF);
-    }
+    uart_putc('H');
+    uart_putc('i');
     uart_putc('\n');
-    
-    // Clear RX
-    RX_READY = 0;
-    RX_LEN = 0;
-    
-    uart_puts("Done!\n");
     
     while(1);
     return 0;
